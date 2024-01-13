da1578f36ed8ff7de3c42fae9decd0004fd5604d
Clavelian <46989875+clavelian@users.noreply.github.com>
2024-01-13 21:17:04 +0800

    TBC

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 // Image slider
-const slides =
+const slides = document.querySelectorAll(".slides img");
+let slideIndex = 0;
+let intervalId = null;
+
+function initializeSlider() {}
+function showSlide(index) {}
+function prevSlide() {}
+function nextSlide() {}
